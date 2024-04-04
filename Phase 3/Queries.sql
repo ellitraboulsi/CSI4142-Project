83967a4cadab5c1fdb433f0f60a53f8f82d15f91
ellitraboulsi <83986919+ellitraboulsi@users.noreply.github.com>
2024-04-04 14:16:01 -0400

    Update Queries.sql

--- Phase 3/Queries.sql
+++ Phase 3/Queries.sql
@@ -1,4 +1,3 @@
---DONE
 -- Roll up: total profit amount of each product category in each country
 SELECT l.country, p.category, SUM(o.profit) AS total_profit
 FROM order_dimension o
@@ -8,7 +7,7 @@ GROUP BY ROLLUP(l.country, p.category)
 HAVING p.category IS NOT NULL AND l.country IS NOT NULL
 ORDER BY l.country ASC, p.category ASC, total_profit ASC;
 
---DONE	
+
 -- Drill Down
 -- Step 1: Aggregate sales data by product category
 SELECT p.category, SUM(o.profit) AS total_profit
@@ -31,7 +30,7 @@ JOIN product_dimension p ON RIGHT(CAST(o.order_key AS TEXT), 4) = RIGHT(CAST(p.p
 GROUP BY p.category, p.sub_category, p.product_name
 ORDER BY p.category ASC, p.sub_category ASC, p.product_name ASC, total_profit DESC;
 
---DONE
+
 -- Slice Query: Compare the profit in California to that only in New York, for all Office Supplies
 SELECT l.state, p.category, SUM(o.profit) AS total_profit
 FROM order_dimension o
@@ -41,7 +40,7 @@ WHERE l.state IN ('California', 'New York')
   AND p.category = 'Office Supplies'
 GROUP BY l.state, p.category;
 
---DONE
+
 -- Dice Query 1: Compare the profit in the United States for Office Supplies versus Technology
 SELECT p.category, SUM(o.profit) AS total_profit
 FROM order_dimension o
@@ -51,7 +50,7 @@ WHERE l.country = 'United States'
   AND p.category IN ('Office Supplies', 'Technology')
 GROUP BY p.category;
 
---DONE
+
 -- Dice Query 2: Total sales amount for the product category 'Technology' in the year 2014 for the markets 'EU' and 'Africa'.
 SELECT p.category, l.market, d.order_year, SUM(o.sales) AS total_sales
 FROM order_dimension o
@@ -66,7 +65,6 @@ GROUP BY p.category, l.market, d.order_year;
 
 -- COMBINED QUERIES
 
---DONE
 -- 1. Roll up and Slice: Analyze the total profit for each product category and year, but only for the United States and Canada
 SELECT p.category, d.order_year, SUM(o.profit) AS total_profit
 FROM sales_fact_table f
@@ -80,7 +78,6 @@ HAVING p.category IS NOT NULL AND d.order_year IS NOT NULL
 ORDER BY p.category ASC, d.order_year ASC;
 
 
---DONE
 -- 2. Compare the sales in France vs Australia for Technology versus Office Supplies in Dec 2011 versus Nov 2011
 SELECT l.country, p.category, d.order_month, d.order_year, SUM(f.total_price) AS total_sales
 FROM sales_fact_table f
@@ -109,8 +106,8 @@ WHERE
 GROUP BY p.category, l.country, d.order_year, d.order_month
 ORDER BY l.country, p.category, d.order_month ASC;
 
---DONE
--- 3. Compare the sales of tables in the United States for February 2013 versus December 2013,
+
+-- 3. Compare the sales of tables in the United States for February 2013 versus December 2013
 SELECT 
     d.order_month,
     d.order_year,
@@ -130,7 +127,7 @@ GROUP BY
 ORDER BY 
     d.order_month ASC, d.order_year;
 
---DONE
+
 -- 4. Analyzing sales data for the Office Supplies and Technology categories in Canadian provinces for the year 2014
 SELECT
     l.state AS province,
@@ -155,7 +152,7 @@ GROUP BY
 HAVING pd.category IS NOT NULL AND l.state IS NOT NULL AND odd.order_year IS NOT NULL
 ORDER BY total_quantity ASC;
 
---DONE
+
 -- Iceberg query: Five countries with the highest sales growth from 2010 to 2011
 SELECT 
     l.country AS country,
@@ -170,7 +167,7 @@ GROUP BY l.country
 ORDER BY sales_growth DESC
 LIMIT 5;
 
---DONE
+
 -- Windowing: Compare each product's average price with the average price in its category
 SELECT 
     p.sub_category, 
@@ -188,7 +185,7 @@ JOIN product_dimension p ON f.product_key = p.product_key
 JOIN order_dimension od ON f.order_key = od.order_key
 GROUP BY p.sub_category, p.product_name;
 
--- DONE
+
 -- Window Clause: Compares the number of orders in European countries in 2013 to that of the previous and next years.
 SELECT order_year, country, num_orders, prev_year_orders, next_year_orders
 FROM (
@@ -205,5 +202,3 @@ FROM (
     GROUP BY d.order_year, l.country
 ) AS order_counts
 WHERE prev_year_orders IS NOT NULL AND next_year_orders IS NOT NULL;
-
-
